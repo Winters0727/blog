0dbe06f3c15ac4d76b82bbdf4872e3fd5b38c2dd
winters <winters0727@gmail.com>
2023-06-16 16:20:33 +0900

    refactor: mongodb 초기화 코드 수정

--- backend/database.ts
+++ backend/database.ts
@@ -1,4 +1,6 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { Document, MongoClient, ServerApiVersion } from "mongodb";
+import ArticleSchema from "./src/models/article.ts";
+import CommentSchema from "./src/models/comment.ts";
 
 const DATABASE_URL = `${process.env.DATABASE_PREFIX}${process.env.DATABASE_ACCOUNT}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_SUFFIX}`;
 
@@ -12,6 +14,32 @@ const client = new MongoClient(DATABASE_URL, {
 
 const blogDatabase = client.db("blog");
 
+const collections = [
+  {
+    name: "article",
+    schema: ArticleSchema,
+  },
+  {
+    name: "comment",
+    schema: CommentSchema,
+  },
+];
+
+const getCollection = (name: string) => blogDatabase.collection(name);
+
+const createCollection = async (name: string, schema: Document) =>
+  await blogDatabase.createCollection(name, {
+    validator: {
+      $jsonSchema: schema,
+    },
+  });
+
+const initializeCollection = async (name: string, schema: Document) => {
+  const dbcollections = await blogDatabase.collections();
+  if (!dbcollections.some((document) => document.collectionName === name))
+    await createCollection(name, schema);
+};
+
 const initializeDatabase = async () => {
   try {
     console.log("Attempt to connect database...");
@@ -19,13 +47,18 @@ const initializeDatabase = async () => {
     await client.connect();
 
     console.log("Database connection is accomplished!");
+
+    console.log("Initializing collections...!");
+
+    for (const { name, schema } of collections) {
+      await initializeCollection(name, schema);
+    }
+
+    console.log("Initialization is completed!");
   } catch (err: any) {
     await client.close();
     console.error(err);
   }
 };
-
-const getCollection = (collection: string) =>
-  blogDatabase.collection(collection);
 
 export { client, initializeDatabase, getCollection };
