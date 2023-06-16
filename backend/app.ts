06a1a3cceaf3d0ee828af531178bc2f47f64443e
winters <winters0727@gmail.com>
2023-06-16 18:57:54 +0900

    chore: body-parser 추가

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 import "./env.ts";
 import { initializeDatabase } from "./database.ts";
 import ArticleRouter from "./src/routes/article.ts";
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use("/article", ArticleRouter);
 
