b48f861786b409dbf393ddfe24b2890b43eeadf0
winters <winters0727@gmail.com>
2024-01-30 05:50:18 +0900

    refactor: 캐릭터 데이터 스키마 수정

--- backend/src/models/api/furuyoni/character.model.ts
+++ backend/src/models/api/furuyoni/character.model.ts
@@ -7,21 +7,17 @@ const characterSchema: ModelSchema = {
       bsonType: "string",
       description: "캐릭터 코드",
     },
-    korName: {
+    korData: {
       bsonType: "object",
-      description: "캐릭터 한글 이름",
+      description: "캐릭터 한글 데이터",
     },
-    engName: {
+    engData: {
       bsonType: "object",
-      description: "캐릭터 영어 이름",
+      description: "캐릭터 영어 데이터",
     },
-    jpnName: {
+    jpnData: {
       bsonType: "object",
-      description: "캐릭터 일본어 이름",
-    },
-    season: {
-      bsonType: "string",
-      description: "캐릭터 등장 시즌",
+      description: "캐릭터 일어 데이터",
     },
     mode: {
       bsonType: "array",
@@ -39,27 +35,11 @@ const characterSchema: ModelSchema = {
       bsonType: "object",
       description: "캐릭터 추가패",
     },
-    abilityKeyword: {
-      bsonType: "string",
-      description: "캐릭터 키워드",
-    },
-    abilityDescription: {
-      bsonType: "string",
-      description: "캐릭터 키워드 설명",
-    },
-    symbolWeapon: {
-      bsonType: "string",
-      description: "캐릭터 상징 무기",
-    },
-    symbolSub: {
-      bsonType: "object",
-      description: "캐릭터 상징 보조 키워드",
-    },
-    created_at: {
+    createdAt: {
       bsonType: "date",
       description: "데이터 생성 날짜",
     },
-    updated_at: {
+    updatedAt: {
       bsonType: "date",
       description: "데이터 수정 날짜",
     },
